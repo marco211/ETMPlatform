4cf57196aaa4e1558bb7f07829143ee7b6a7ce32
Alfonso <alfonsoambrosino1997@gmail.com>
2018-12-03 14:25:50 +0100

    alfonso
    connessione al database
    
    Co-Authored-By: kill-alf <kill-alf@users.noreply.github.com>
    Co-Authored-By: francescodrag <francescodrag@users.noreply.github.com>

---
+++ src/it/unisa/etm/connessioneDB/DatabaseManager.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+	private static DatabaseManager istanza=null;
+	private static Connection connection;
+	private DatabaseManager(){
+		
+	}
+	
+	public static synchronized DatabaseManager getIstance() throws SQLException {
+		if(istanza==null) {
+			istanza=new DatabaseManager();
+			connection=createConnection();
+		}
+		return istanza;
+	}
+	
+	public synchronized Connection getConnection() {
+		return connection;
+	}
+	
+	private static Connection createConnection() throws SQLException {
+		String ip = "localhost";
+		String port = "3306";
+		String db = "ETM_PLATFORM";
+		String username = "";//inserite la vostra username
+		String password = "";//inserite la vostra password
+		connection = DriverManager.getConnection("jdbc:mysql://" + ip + ":" + port + "/" + db + "?zeroDateTimeBehavior=convertToNull", username, password);
+		System.out.println("**** Connessione creata con successo ****" + connection.toString() + "\n");
+		return connection;
+	}
+}
