619c9a1e47dea141723b4eaaa885c4e9f2b95eb0
Alfonso <alfonsoambrosino1997@gmail.com>
2018-12-07 11:34:38 +0100

    Modifiche classe DatabaseManager
    Co-Authored-By: kill-alf <kill-alf@users.noreply.github.com>
    Co-Authored-By: francescodrag <francescodrag@users.noreply.github.com>

--- src/it/unisa/etm/database/DatabaseManager.java
+++ src/it/unisa/etm/database/DatabaseManager.java
@@ -3,34 +3,27 @@ package it.unisa.etm.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class DatabaseManager {
-	private static DatabaseManager istanza=null;
-	private static Connection connection;
+	private static Connection connection=null;
 	private DatabaseManager(){
 		
 	}
 	
-	public static synchronized DatabaseManager getInstance() throws SQLException {
-		if(istanza==null) {
-			istanza=new DatabaseManager();
-			connection=createConnection();
+	public static Connection getIstance() throws SQLException {
+		if(connection==null) {
+			createConnection();
 		}
-		return istanza;
-	}
-	
-	public Connection getConnection() {
 		return connection;
 	}
 	
-	private static Connection createConnection() throws SQLException {
-		String ip = "localhost";
-		String port = "3306";
-		String db = "ETM_PLATFORM";
-		String username = "";//inserite la vostra username
-		String password = "";//inserite la vostra password
-		connection = DriverManager.getConnection("jdbc:mysql://" + ip + ":" + port + "/" + db + "?zeroDateTimeBehavior=convertToNull", username, password);
-		System.out.println("**** Connessione creata con successo ****" + connection.toString() + "\n");
-		return connection;
+
+	
+	private static void createConnection() throws SQLException {
+		String username = "etm";//inserite la vostra username
+		String password = "Etm1";//inserite la vostra password
+		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ETM_PLATFORM?zeroDateTimeBehavior=convertToNull", username, password);
+		Logger.getLogger("global").info("**** Connessione creata con successo **** " + connection.toString() + "\n");		
 	}
 }
