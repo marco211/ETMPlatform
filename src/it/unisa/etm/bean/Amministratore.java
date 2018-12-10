2d5a17cc38849b355929f3ac51a768781b80f0da
Spineshankumb <carpioumberto@gmail.com>
2018-12-10 15:07:53 +0100

    Update Amministratore.java
    Aggiunta classe

--- src/it/unisa/etm/bean/Amministratore.java
+++ src/it/unisa/etm/bean/Amministratore.java
@@ -1,5 +1,72 @@
 package it.unisa.etm.bean;
 
-public class Amministratore {
+import java.io.Serializable;
+import java.sql.Date;
 
+public class Amministratore implements Serializable	 {
+	
+	public Amministratore() {
+		
+	}
+
+	public Amministratore(String cognome, Date dataDiNascita, String nome, String email,String password) {
+		super();
+		this.cognome = cognome;
+		this.nome = nome;
+		this.email = email;
+		this.password = password;
+	}
+
+
+	public String getCognome() {
+		return cognome;
+	}
+
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public static Amministratore loginUtente(String email, String password) {
+		return null;
+	}
+	
+	public static void aggiungiUtente(Utente utente) {
+		
+	}
+
+	private String cognome;
+	private String nome;
+	private String email;
+	private String password;
 }
