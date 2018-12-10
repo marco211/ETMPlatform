225e3f4197234b20c0ef9979812a360e46e5d9c0
dnappi97 <dnappi89@gmail.com>
2018-12-10 16:36:13 +0100

    interfacce e managere delle classi entity

---
+++ src/it/unisa/etm/model/PropostaTesiManager.java
@@ -0,0 +1,70 @@
+package it.unisa.etm.model;
+
+import java.util.ArrayList;
+
+public class PropostaTesiManager implements PropostaTesiModelInterface {
+
+	public PropostaTesiManager() {
+		
+	}
+	@Override
+	public void aggiungiPropostaTesi(Object propostaTesi) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void archiviaPropostaTesi(Object propostaTesi) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object cercaPropostaTesi(String titolo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void chiudiPropostaTesi(Object propostaTesi) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean comunicaEsito(Object propostaTesi) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void inviaPropostaTesi(Object propostaTesi) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificaPropostaTesi(Object propostaTesi) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList proposteTesiAttive() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void rimuoviPropostaTesi(Object propostaTesi) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String visualizzaDettagliPropostaTesi(Object propostaTesi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
