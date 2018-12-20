65e7ca7ff2ac8602fdf47c245263ac05689accbd
marco211 <mar.dellamedaglia@gmail.com>
2018-12-20 17:42:03 +0100

    Bean amministratore

---
+++ src/it/unisa/etm/bean/Amministratore.java
@@ -0,0 +1,36 @@
+package it.unisa.etm.bean;
+
+public class Amministratore {
+
+	public Amministratore(String nome, String cognome, String email) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.email = email;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	private String nome, cognome, email;
+}
