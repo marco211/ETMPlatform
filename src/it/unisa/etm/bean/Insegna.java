a0ed1316c624eb141f4fc664fa7e7155e94da8f1
francescodrag <francescodragone@live.it>
2018-12-20 16:52:30 +0100

    mammt

--- src/it/unisa/etm/bean/Insegna.java
+++ src/it/unisa/etm/bean/Insegna.java
@@ -1,5 +1,35 @@
 package it.unisa.etm.bean;
 
 public class Insegna {
+	
+	public Insegna() {
+		
+	}
+	
+	public Insegna(String utenteEmail, String insegnamentoNome) {
+		super();
+		this.utenteEmail = utenteEmail;
+		this.insegnamentoNome = insegnamentoNome;
+	}
 
+	public String getUtenteEmail() {
+		return utenteEmail;
+	}
+
+	public void setUtenteEmail(String utenteEmail) {
+		this.utenteEmail = utenteEmail;
+	}
+
+	public String getInsegnamentoNome() {
+		return insegnamentoNome;
+	}
+
+	public void setInsegnamentoNome(String insegnamentoNome) {
+		this.insegnamentoNome = insegnamentoNome;
+	}
+
+
+
+	private String utenteEmail;
+	private String insegnamentoNome;
 }
