01c359cec3625785b471573f9c5a3eb3cccf844d
andreaminichino <45553868+andreaminichino@users.noreply.github.com>
2018-12-21 22:02:19 +0100

    aggiunta lista e richiesta proposta tesi

--- src/it/unisa/etm/tesi/InviaPropostaTesiServlet.java
+++ src/it/unisa/etm/tesi/InviaPropostaTesiServlet.java
@@ -1,13 +1,22 @@
 package it.unisa.etm.tesi;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import it.unisa.etm.bean.PropostaTesi;
+import it.unisa.etm.bean.RichiestaPartecipazione;
+import it.unisa.etm.bean.Utente;
+import it.unisa.etm.factory.ManagerFactory;
+import it.unisa.etm.model.manager.PropostaTesiManager;
+import it.unisa.etm.model.manager.UtenteManager;
 
 /**
  * Estende la classe HttpServlet ed offre all'utente registrato come studente la funzionalità di poter inviare all'utente registrato come docente una richiesta di tesi per una determinata proposta tesi.
@@ -28,8 +37,19 @@ public class InviaPropostaTesiServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int id=Integer.parseInt(request.getParameter("id_richiesta"));
+		LocalDate data=LocalDate.parse(request.getParameter("data"));
+		int propostatesi_id=Integer.parseInt(request.getParameter("propostatesi_id"));
+		String utente_mail=request.getParameter("utente_mail");
+		
+		RichiestaPartecipazione richiesta = new RichiestaPartecipazione(id, data, propostatesi_id, utente_mail);
+		
+		if(inviaRichiestaPropostaTesi(richiesta));
+		{
+			HttpSession session=request.getSession();
+			session.setAttribute("richiesta", richiesta);
+		}
+		//request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
 	/**
@@ -47,8 +67,15 @@ public class InviaPropostaTesiServlet extends HttpServlet {
 	 * <p>
 	 * false se non è avvenuto con successo.
 	 */
-	private boolean inviaRichiestaPropostaTesi(PropostaTesi richiesta){
-		return false;
-		
+	private boolean inviaRichiestaPropostaTesi(RichiestaPartecipazione richiesta){ //cambiato il parametro da PropostaTesi a RichiestaPartecipazione
+		ManagerFactory mf=new ManagerFactory();
+		PropostaTesiManager ptm=(PropostaTesiManager) mf.createPropostaTesiManager();
+		try {
+			ptm.inserisciRichiestaPartecipazione(richiesta);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;		
 	}
 }
---
+++ src/it/unisa/etm/tesi/ListaProposteTesiAttiveServlet.java
@@ -0,0 +1,58 @@
+package it.unisa.etm.tesi;
+
+import java.io.IOException;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import it.unisa.etm.model.interfaces.*;
+import it.unisa.etm.model.manager.*;
+import it.unisa.etm.bean.PropostaTesi;
+
+/**
+ * Servlet implementation class ListaProposteTesiAttive
+ */
+@WebServlet("/ListaProposteTesiAttive")
+public class ListaProposteTesiAttiveServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private PropostaTesiManager  propostamanager;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListaProposteTesiAttiveServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try {
+			System.out.println("Mammt");
+			propostamanager = new PropostaTesiManager();
+			ArrayList<PropostaTesi> proposte = propostamanager.getProposteTesiAttive();
+			request.setAttribute("proposte", proposte);
+			request.getRequestDispatcher("listaProposteTesiAttive.jsp").forward(request, response);
+		}catch(SQLException e){
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
