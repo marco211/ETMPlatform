6627a5dfa1a8bff1d8b44749d6a03bd51999b222
francescodrag <francescodragone@live.it>
2018-12-21 15:54:33 +0100

    yt

--- src/it/unisa/etm/model/interfaces/ConsegnaModelInterface.java
+++ src/it/unisa/etm/model/interfaces/ConsegnaModelInterface.java
@@ -1,6 +1,7 @@
 package it.unisa.etm.model.interfaces;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import it.unisa.etm.bean.Consegna;
 
@@ -10,4 +11,5 @@ public interface ConsegnaModelInterface {
 	public boolean modificaConsegna(Consegna c) throws SQLException;
 	public boolean eliminaConsegna(int id) throws SQLException;
 	public Consegna getConsegna(int id) throws SQLException;
+	public ArrayList<Consegna> getListaConsegne(int propostaTesiId) throws SQLException;
 }
