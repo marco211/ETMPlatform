4fdebabe92faedf731e81d6912eea089ad8aedbd
andreaminichino <45553868+andreaminichino@users.noreply.github.com>
2018-12-22 22:08:06 +0100

    modifica jsp aggiungi prop tesi & altro

--- src/it/unisa/etm/database/DatabaseManager.java
+++ src/it/unisa/etm/database/DatabaseManager.java
@@ -27,8 +27,8 @@ import java.util.logging.Logger;
 		private static void createConnection() throws SQLException, ClassNotFoundException {
 			Class.forName("com.mysql.jdbc.Driver");
 			String username = "root";//inserite la vostra username
-			String password = "admin";//inserite la vostra password
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ETM_PLATFORM?zeroDateTimeBehavior=convertToNull", username, password);
+			String password = "root";//inserite la vostra password
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ETM_PLATFORM?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", username, password);
 			Logger.getLogger("global").info("**** Connessione creata con successo **** " + connection.toString() + "\n");		
 		}
 	}
