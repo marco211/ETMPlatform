8bbe6eea3db8172fd48be78cfadd556e0f965991
marco211 <mar.dellamedaglia@gmail.com>
2018-12-25 12:33:39 +0100

    piccole modifiche registrazione

--- src/it/unisa/etm/autenticazione/RegistrazioneServlet.java
+++ src/it/unisa/etm/autenticazione/RegistrazioneServlet.java
@@ -30,20 +30,14 @@ import it.unisa.etm.model.manager.UtenteManager;
 @WebServlet("/RegistrazioneServlet")
 public class RegistrazioneServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegistrazioneServlet() {
-        super();
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
+	public RegistrazioneServlet() {
+		super();
 	}
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -56,20 +50,22 @@ public class RegistrazioneServlet extends HttpServlet {
 		String data=request.getParameter("data");
 		String tipo=request.getParameter("tipo");
 		String validazione=UUID.randomUUID().toString();
-		Utente utente;
-		if(tipo.equals("s"))
-		{
+		Utente utente = null;
+
+
+		if(tipo.equals("s")){
+
 			long matricola=Long.parseLong(request.getParameter("matricola"));
 			utente=new Utente(cognome, data, nome, tipo, email, password, matricola, validazione);
-		}
-		else
-		{
+
+		}else{
+
 			String insegnamento=request.getParameter("insegnamento").toLowerCase();
 			String ufficio=request.getParameter("ufficio");
 			utente=new Utente(cognome, data, ufficio, tipo, nome, email, password, insegnamento, validazione);
 		}
-		String testo="<!DOCTYPE html>\r\n" + 
-				"<html>\r\n" + 
+
+		String testo="<html>\r\n" + 
 				"<head>\r\n" + 
 				"<meta charset=\"ISO-8859-1\">\r\n" + 
 				"<title>ETM Platform</title>\r\n" + 
@@ -77,22 +73,18 @@ public class RegistrazioneServlet extends HttpServlet {
 				"<body>\r\n" + 
 				"\r\n" + 
 				"copia il link in descrizione ed aprilo per confermare registrazione <br>" +
-				"\"localhost:8080/ETMPlatform/ConfermaMailServlet?validazione=" + validazione + "\"" + 
+				"<a href=\"http://localhost:8080/ETMPlatform/ConfermaMailServlet?validazione=" + validazione + "\"" + ">Clicca qui</a>" +
 				"\r\n" + 
 				"</body>\r\n" + 
 				"</html>";
-		
-		if(registrazioneControl(utente))
-		{
-			try {
-				send(email, testo);
-			} catch (AddressException e) {
-				e.printStackTrace();
-			} catch (MessagingException e) {
-				e.printStackTrace();
+
+		if(registrazioneControl(utente)) {
+			if(sendEmail(email, testo)) {
+				response.sendRedirect(request.getContextPath()+"/index.jsp");
+			}else {
+				//se il metodo ritorna false c'è stato un errore, gestirlo con altra jsp, servlet o messaggio di errore
 			}
 		}
-		response.sendRedirect(request.getContextPath()+"/index.jsp");
 	}
 
 
@@ -115,39 +107,60 @@ public class RegistrazioneServlet extends HttpServlet {
 		}
 		return true;
 	}
-	
-	
-	public static void send(String ricevente, String testo) throws AddressException, MessagingException {
-        Properties prop = System.getProperties();
-        
-        prop.setProperty("mail.transport.protocol", "smtp");
-        prop.put("mail.smtp.auth", "true");
+
+
+	public static boolean sendEmail(String ricevente, String testo){
+		Properties prop = System.getProperties();
+
+		prop.setProperty("mail.transport.protocol", "smtp");
+		prop.put("mail.smtp.auth", "true");
 		prop.put("mail.smtp.starttls.enable", "true");
 		prop.put("mail.smtp.host", "smtp.gmail.com");
 		prop.put("mail.smtp.port", "587");
 
-        Session session = Session.getDefaultInstance(prop, null);
-        Message msg = new MimeMessage(session);
-        InternetAddress addForm;
+		Session session = Session.getDefaultInstance(prop, null);
+		Message msg = new MimeMessage(session);
+		InternetAddress sender = null;
+		InternetAddress receiver = null;
+
 		try {
-			addForm = new InternetAddress("emtplatform@gmail.com", "etm platform");
-			msg.setFrom(addForm);
+			sender = new InternetAddress("emtplatform@gmail.com", "etm platform");
+			receiver = new InternetAddress(ricevente, "Email ricevente");
+
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return false;
 		}
-        
 
-        InternetAddress addTo = new InternetAddress(ricevente);
 
-        msg.setRecipient(Message.RecipientType.TO, addTo);
-        msg.setSubject("Conferma registrazione"); 
-        msg.setContent(testo, "text/html"); 
-        
-        
-        Transport transport=session.getTransport();
-        transport.connect("smtp.gmail.com", "etmplatform@gmail.com", "Prova1234"); 
-        transport.sendMessage(msg, msg.getAllRecipients());
-    }
-	
+
+		try {
+			msg.setFrom(sender);
+			msg.setRecipient(Message.RecipientType.TO, receiver);
+			msg.setSubject("Conferma registrazione"); 
+			msg.setContent(testo, "text/html"); 
+		} catch (MessagingException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+
+
+		
+		
+		try {
+			
+			Transport transport=session.getTransport();
+			transport.connect("smtp.gmail.com", "etmplatform@gmail.com", "Prova1234");
+			transport.sendMessage(msg, msg.getAllRecipients());
+			
+		} catch (MessagingException e) {
+			e.printStackTrace();
+			return false;
+		} 
+		
+
+		return true;
+	}
+
 }
