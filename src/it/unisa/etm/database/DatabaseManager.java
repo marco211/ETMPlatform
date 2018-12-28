11eb06f0f589b9be623db4bed9c933719c1ab336
Alfonso <alfonsoambrosino1997@gmail.com>
2018-12-28 17:33:22 +0100

    DB manager

--- src/it/unisa/etm/database/DatabaseManager.java
+++ src/it/unisa/etm/database/DatabaseManager.java
@@ -27,11 +27,7 @@ import java.util.logging.Logger;
 		private static void createConnection() throws SQLException, ClassNotFoundException {
 			Class.forName("com.mysql.jdbc.Driver");
 			String username = "root";//inserite la vostra username
-<<<<<<< HEAD
-			String password = "854621abC";//inserite la vostra password
-=======
-			String password = "root";//inserite la vostra password
->>>>>>> 30ed679b5ece67548ab315d0bd988d1d3f8fb90a
+			String password = "";//inserite la vostra password
 			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ETM_PLATFORM?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", username, password);
 			Logger.getLogger("global").info("**** Connessione creata con successo **** " + connection.toString() + "\n");		
 		}
