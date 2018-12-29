40a049c5c7004d0e79b7a662fdb6905c33a7e8c6
Alfonso <alfonsoambrosino1997@gmail.com>
2018-12-29 16:50:31 +0100

    Consegne terminate

--- WebContent/js/consegna.js
+++ WebContent/js/consegna.js
@@ -1,5 +1,5 @@
 function scadenza(){
 	document.getElementById("scadenzaConsegna").className = "d-none";
-	document.getElementById("modificaConsegnaDiv").className = "form-group d-block";
+	document.getElementById("modificaConsegnaDiv").className = "d-block";
 	
 }
