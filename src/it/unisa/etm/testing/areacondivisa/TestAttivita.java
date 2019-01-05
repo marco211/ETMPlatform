633415f4cd042aa8c7091d84c6c99d73f932125a
marco211 <mar.dellamedaglia@gmail.com>
2019-01-05 12:34:17 +0100

    Esempi JUnit

--- src/it/unisa/etm/testing/areacondivisa/TestAttivita.java
+++ src/it/unisa/etm/testing/areacondivisa/TestAttivita.java
@@ -1,66 +1,72 @@
 package it.unisa.etm.testing.areacondivisa;
 
 import java.time.LocalDate;
-
 import it.unisa.etm.bean.Attivita;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
 public class TestAttivita {
-	private Attivita attivita;
 	
-	public TestAttivita() {
-		
+	private Attivita attivitaOk;
+	private Attivita attivitaNotOk;
+	
+	@AfterAll
+	public void setUp() {
+	
 	}
 	
-	public TestAttivita(Attivita attivita) {
-		this.attivita=attivita;
-	}
-	
-	public int testGetId() {
-		return attivita.getId();
+	@Test
+	void testGetId() {
+		int id = attivitaOk.getId();
+		assertEquals(id, 1); //ad esempio, se 1 è l'id settato in set up.. 
+		//poi fate anche qualche assert false per vedere sempre se va bene (con attivitaNotOk per esempio)
 	}
 
-	public void testSetId(int id) {
-		attivita.setId(id);
+	void testSetId(int id) {
+		attivitaOk.setId(id);
 	}
 
-	public String testGetUtente_Email() {
-		return attivita.getUtente_Email();
+	public void testGetUtente_Email() {
+		String email = attivitaOk.getUtente_Email();
 	}
 
 	public void testSetUtente_Email(String utente_Email) {
-		attivita.setUtente_Email(utente_Email);	
+		attivitaOk.setUtente_Email(utente_Email);	
 	}
 
 	public String testGetNomeFile() {
-		return attivita.getNomeFile();
+		return attivitaOk.getNomeFile();
 	}
 
 	public void testSetNomeFile(String nomeFile) {
-		attivita.setNomeFile(nomeFile);
+		attivitaOk.setNomeFile(nomeFile);
 	}
 
 	public LocalDate testGetData() {
-		return attivita.getData();
+		return attivitaOk.getData();
 	}
 
 	public void testSetData(LocalDate data) {
-		attivita.setData(data);
+		attivitaOk.setData(data);
 	}
 
 	public String testGetTipo() {
-		return attivita.getTipo();
+		return attivitaOk.getTipo();
 	}
 
 	public void testSetTipo(String tipo) {
-		attivita.setTipo(tipo);
+		attivitaOk.setTipo(tipo);
 	}
 
 	public int testGetPropostatesi_id() {
-		return attivita.getPropostatesi_id();
+		return attivitaOk.getPropostatesi_id();
 	}
 
 	public void testSetPropostatesi_id(int propostatesi_id) {
-		attivita.setPropostatesi_id(propostatesi_id);;
+		attivitaOk.setPropostatesi_id(propostatesi_id);;
 	}
 	
 		
